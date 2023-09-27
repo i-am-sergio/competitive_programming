354532a68b95eb4f09fc040968a82e6c58458359
sergio15dev <sergio15.dev@gmail.com>
2023-09-27 17:40:28 -0500

    contest-27sep

---
+++ contest-27sep/ejercicio1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int masCercanoIzq(string &linea, int pos)
+{
+    int contadorIzq = 0;
+    int i = pos - 1;
+    while (i >= 0)
+    {
+        if (linea[i] == 'X')
+            break;
+        i--;
+        contadorIzq++;
+    }
+    return contadorIzq;
+}
+
+int masCercanoDer(string &linea, int pos)
+{
+    int contadorDer = 0;
+    int i = pos + 1;
+    while (i < linea.size())
+    {
+        if (linea[i] == 'X')
+            break;
+        i++;
+        contadorDer++;
+    }
+    return contadorDer;
+}
+
+int masCercano(string &linea, int &pos)
+{
+    int distancia;
+    if (pos == 0)
+    {
+        return masCercanoDer(linea, pos);
+    }
+    else if (pos == linea.size() - 1)
+    {
+        return masCercanoIzq(linea, pos);
+    }
+
+    int izq = masCercanoIzq(linea, pos);
+    int der = masCercanoDer(linea, pos);
+    return (izq <= der) ? izq : der;
+}
+
+int main()
+{
+    std::string linea;
+    int solucionMax = 0;
+    int solucionTemp = 0;
+
+    while (cin >> linea)
+    {
+        cout << linea << endl;
+        for (int i = 0; i < linea.size(); i++)
+        {
+            if (linea[i] != 'X')
+                solucionTemp = masCercano(linea, i);
+
+            if (solucionTemp > solucionMax)
+                solucionMax = solucionTemp;
+            // cout << "solucionTemp -> " << solucionTemp << "\n";
+            // cout << "solucionMax -> " << solucionMax << "\n";
+        }
+
+        cout << solucionMax << endl;
+        solucionMax = 0;
+        solucionTemp = 0;
+    }
+
+    return 0;
+}
---
+++ contest-27sep/ejercicio2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+void algo()
+{
+    std::cout << "aaa\n";
+}
+
+int main()
+{
+    int T;
+    std::cin >> T;
+    int casos = 0;
+    std::string entrada;
+
+    while (casos <= T)
+    {
+        std::cin >> entrada;
+        algo();
+        casos++;
+    }
+
+    return 0;
+}
