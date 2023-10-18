73ed7ae29f9d0e8bf46cfb9661d6e94274e68d3a
sergio15dev <sergio15.dev@gmail.com>
2023-10-17 23:25:15 -0500

    add 3 brothers

--- contest-27sep/3_Brothers.cpp
+++ contest-27sep/3_Brothers.cpp
@@ -1,16 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fast()
+void fazt()
 {
     ios::sync_with_stdio(false);
-    cin.tie();
-    cout.tie(NULL);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 }
 
-int main()
-{
-    fast();
+void batalla(vector<vector<int>>& ini, int n,int r, int c){
+    vector<vector<int>> res = ini;
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            if(j+1<c){ // 2 0
+                if(ini[i][j+1] == ini[i][j]+1 || ini[i][j+1]+n == ini[i][j]+1){ // der
+                    res[i][j+1] = ini[i][j];
+                }
+            }
+            if(j-1 >= 0){
+                if(ini[i][j-1] == ini[i][j]+1 || ini[i][j-1]+n == ini[i][j]+1){ // izq
+                    res[i][j-1] = ini[i][j];
+                }
+            }
+            if(i+1 < r){
+                if(ini[i+1][j] == ini[i][j]+1 || ini[i+1][j]+n == ini[i][j]+1){ // abajo
+                    res[i+1][j] = ini[i][j];
+                }
+            }
+            if(i-1 >= 0){
+                if(ini[i-1][j] == ini[i][j]+1 || ini[i-1][j]+n == ini[i][j]+1){ // arriba
+                    res[i-1][j] = ini[i][j];
+                }
+            }
+        }
+    }
+    ini = res;
+}
 
+int main(){
+    fazt();
+    int n,r,c,k;
+    while (true) {
+        cin >> n >> r >> c >> k;
+        if(n==0 && r==0 && c==0 && k==0) {
+            break;
+        }
+        vector<vector<int>> inicio(r,vector<int>(c));
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                cin >> inicio[i][j];
+            }
+        }
+        for(int i=0; i<k; i++){
+            batalla(inicio,n,r,c);
+        }
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                cout<<inicio[i][j];
+                if(j!=c-1)
+                    cout<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    
     return 0;
 }
